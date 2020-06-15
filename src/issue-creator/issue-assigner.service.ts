0dafeb21d49fbe92eb43987b584ae2a36c3b4750
Jiaxuan He <jiaxuan_he@yahoo.de>
2020-06-15 10:47:25 +0200

    temporary implementation

--- src/issue-creator/issue-assigner.service.ts
+++ src/issue-creator/issue-assigner.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { LogMessageFormat } from 'src/LogMessageFormat';
 import { LogType } from 'src/LogType';
+import { CpuIssueCreatorService } from './cpu-issue-creator.service';
+import { ErrorResponseIssueCreatorService } from './error-response-issue-creator.service';
+import { TimeoutIssueCreatorService } from './timeout-issue-creator.service';
+import { CbOpenIssueCreatorService } from './cb-open-issue-creator.service';
 
 /**
  * This service assigns the log messages from the log receiver
@@ -8,6 +12,12 @@ import { LogType } from 'src/LogType';
  */
 @Injectable()
 export class IssueAssignerService {
+  constructor(
+    private cpuIssueCreator: CpuIssueCreatorService,
+    private errorIssueCreator: ErrorResponseIssueCreatorService,
+    private timeoutIssueCreator: TimeoutIssueCreatorService,
+    private cbIssueCreator: CbOpenIssueCreatorService,
+  ) {}
   /**
    * takes over the log message from the log receiver
    * and assigns it to respective issue creator component
@@ -16,5 +26,20 @@ export class IssueAssignerService {
    */
   takeLogAndAssign(logMsg: LogMessageFormat, logType: LogType): void {
     //determines which issue creator component to send the log to
+    //note, this is just a placeholder for future, improved implementations
+    switch (logType) {
+      case LogType.Cpu: {
+        this.cpuIssueCreator.createIssueFromLog(logMsg);
+      }
+      case LogType.Error: {
+        this.errorIssueCreator.createIssueFromLog(logMsg);
+      }
+      case LogType.Timeout: {
+        this.timeoutIssueCreator.createIssueFromLog(logMsg);
+      }
+      case LogType.CBOpen: {
+        this.cbIssueCreator.createIssueFromLog(logMsg);
+      }
+    }
   }
 }
