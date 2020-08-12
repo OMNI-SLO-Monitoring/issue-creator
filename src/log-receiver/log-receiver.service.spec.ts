59fbd56fcb4ba4d176665fbb949098ad1d3f4a73
Jacky20000 <dao.jackylong@gmail.com>
2020-08-12 23:06:19 +0200

    Revert "Merge branch 'SLADIM-147' into SLADIM-101"
    This reverts commit 557507a2012d764e7b23da9cfda862d80b49daed, reversing
    changes made to b6a778f3d9b5fa1ea4a6ca512e9a54b247cb14e3.

--- src/log-receiver/log-receiver.service.spec.ts
+++ src/log-receiver/log-receiver.service.spec.ts
@@ -1,23 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LogReceiverService } from './log-receiver.service';
-import { HttpModule } from '@nestjs/common';
-import { getModelToken } from '@nestjs/mongoose';
-import { LogMessageFormat, LogType } from 'logging-format';
-import { dbMock } from '../db-mock-data/database-mock';
+import { LogType, LogMessageFormat } from 'logging-format';
 
 describe('LogReceiverService', () => {
   let service: LogReceiverService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
-      providers: [
-        LogReceiverService,
-        {
-          provide: getModelToken('logs'),
-          useValue: dbMock,
-        },
-      ],
+      providers: [LogReceiverService],
     }).compile();
 
     service = module.get<LogReceiverService>(LogReceiverService);
@@ -27,45 +17,16 @@ describe('LogReceiverService', () => {
     expect(service).toBeDefined();
   });
 
-  /**
-   * Test function that probes if valid logs are added to database and
-   * returned. In this case the test should be successful.
-   */
-  it('should add log message to database successfully', async () => {
-    const logMock: LogMessageFormat = {
-      type: LogType.CB_OPEN,
-      time: Date.now(),
-      source: 'Database Service',
-      detector: 'Price Service',
-      data: {
-        failedResponses: 31,
-        openTime: 10,
-      },
+  it('should log "Handling Issue" and "Reporting  Issue"', () => {
+    const testLog: LogMessageFormat = {
+      source: 'asd',
+      detector: 'asdd',
+      time: 23,
+      type: LogType.CPU,
+      message: 'asda',
+      data: null,
     };
-    expect(await service.addLogMessageToDatabase(logMock)).toBe(logMock);
-  });
 
-  /**
-   * Test function that probes whether all predefined logs from
-   * the mock database are returned correctly and fulfill the following
-   * checks. In this case the checks are successful.
-   */
-  it('should return all logs and pass all checks', async () => {
-    let fetchedLogs = await service.getAllLogs();
-    expect(fetchedLogs.length).toBe(2);
-    expect(fetchedLogs[0].type).toBe(LogType.TIMEOUT);
-    expect(fetchedLogs[1].type).toBe(LogType.CPU);
-  });
-
-  /**
-   * Test function that probes whether all predefined logs from
-   * the mock database are returned correctly and fulfill the following
-   * checks. In this case the checks are not successful.
-   */
-  it('should return all logs and fail all checks', async () => {
-    let fetchedLogs = await service.getAllLogs();
-    expect(fetchedLogs.length == 1).toBeFalsy();
-    expect(fetchedLogs[0].type === LogType.ERROR).toBeFalsy();
-    expect(fetchedLogs[1].type === LogType.CB_OPEN).toBeFalsy();
+    expect(service.handleLogMessage(testLog)).toBeDefined();
   });
 });
--- src/log-receiver/log-receiver.service.ts
+++ src/log-receiver/log-receiver.service.ts
@@ -1,14 +1,13 @@
-import { Injectable, Logger, Inject, HttpService } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 import { LogMessageFormat } from 'logging-format';
 import { LogType } from 'logging-format';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { CpuUtilizationIssueCreatorComponent } from '../issue-creator/cpu-issue-creator';
 import { TimeoutIssueCreatorComponent } from '../issue-creator/timeout-issue-creator';
 import { CbOpenIssueCreatorComponent } from '../issue-creator/cp-open-issue-creator';
-import { ErrorResponseIssueCreatorComponent } from '../issue-creator/error-response-issue-creator';
+import { ErrorResponseIssueCreatorComponent } from '../issue-creator/error-response-issue-ceator';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Logs } from 'src/schema/logs.schema';
+import { Model } from "mongoose";
+import { Logs } from "src/schema/logs.schema";
 
 /**
  * This service handles the log message passed down from the controller
@@ -17,64 +16,69 @@ import { Logs } from 'src/schema/logs.schema';
  */
 @Injectable()
 export class LogReceiverService {
+
   // Issue Creator for the Log Types
   cpuUtilizationIssueCreator: CpuUtilizationIssueCreatorComponent;
   timeoutIssueCreator: TimeoutIssueCreatorComponent;
   cbOpenIssueCreator: CbOpenIssueCreatorComponent;
   errorResponseIssueCreator: ErrorResponseIssueCreatorComponent;
-
   constructor(
     private http: HttpService,
     @InjectModel('logs') private logModel: Model<Logs>,
   ) {
     // Create an Issue Creator for each LogType
-    this.cpuUtilizationIssueCreator = new CpuUtilizationIssueCreatorComponent(
-      http,
-    );
+    this.cpuUtilizationIssueCreator = new CpuUtilizationIssueCreatorComponent(http);
     this.timeoutIssueCreator = new TimeoutIssueCreatorComponent(http);
     this.cbOpenIssueCreator = new CbOpenIssueCreatorComponent(http);
-    this.errorResponseIssueCreator = new ErrorResponseIssueCreatorComponent(
-      http,
-    );
+    this.errorResponseIssueCreator = new ErrorResponseIssueCreatorComponent(http);
   }
 
   /**
    * Handling of Log messages
-   *
    * @param logMessage is the log received by the log receiver controller
    * This calls the handleLog of the corresponding IssueCreator and passed the log message
    */
-  handleLogMessage(logMessage: LogMessageFormat) {
+  async handleLogMessage(logMessage: LogMessageFormat) {
+    let issueID;
     switch (logMessage.type) {
       case LogType.CPU:
-        this.cpuUtilizationIssueCreator.handleLog(logMessage);
+        issueID = await this.cpuUtilizationIssueCreator.handleLog(logMessage);
         break;
       case LogType.CB_OPEN:
-        this.cbOpenIssueCreator.handleLog(logMessage);
+        issueID = await this.cbOpenIssueCreator.handleLog(logMessage);
         break;
       case LogType.ERROR:
-        this.errorResponseIssueCreator.handleLog(logMessage);
+        issueID = await this.errorResponseIssueCreator.handleLog(logMessage);
         break;
       case LogType.TIMEOUT:
-        this.timeoutIssueCreator.handleLog(logMessage);
+        issueID = await this.timeoutIssueCreator.handleLog(logMessage)
         break;
       default:
-        throw 'Not Implemented LogType';
+        throw "Not Implemented LogType"
     }
+    return issueID;
   }
   /**
-   * Writes the received log message into the database
-   *
+   * Writes the received log message into the database together with the issue ID
+   * 
    * @param logMessage Log sent by the monitor
    */
   async addLogMessageToDatabase(logMessage: LogMessageFormat): Promise<Logs> {
-    const addedLog = new this.logModel(logMessage);
+    const issueID = await this.handleLogMessage(logMessage);
+    const log = {
+      time: logMessage.time,
+      source: logMessage.source,
+      detector: logMessage.detector,
+      message: logMessage.message,
+      type: logMessage.type,
+      data: logMessage.data,
+      issueID: issueID
+    }
+    const addedLog = new this.logModel(log);
     return addedLog.save();
   }
   /**
    * Gets all logs from the database
-   *
-   * @returns all logs from the database
    */
   async getAllLogs(): Promise<Logs[]> {
     return this.logModel.find().exec();
