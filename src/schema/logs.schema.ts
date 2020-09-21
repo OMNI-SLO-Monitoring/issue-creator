cdbbcf0375225f2359b3f6145ff4b51b46fe1b53
trod <st104680@stud.uni-stuttgart.de>
2020-09-21 10:54:56 +0200

    Code cleanup and comments

--- src/schema/logs.schema.ts
+++ src/schema/logs.schema.ts
@@ -2,26 +2,36 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { LogType } from 'logging-format';
 
+/**
+ * Schema for the log messages to be used by mongoose
+ */
 @Schema()
 export class Logs extends Document {
+  /** url of source */
   @Prop()
   sourceUrl: string;
 
+  /** url of detector */
   @Prop()
   detectorUrl: string;
 
+  /** time when log was reported */
   @Prop()
   time: number;
 
+  /** message of log */
   @Prop()
   message: string;
 
+  /** type of the log */
   @Prop()
   type: LogType;
 
+  /** data of the log */
   @Prop()
   data: any;
-
+  
+  /** issueID of the log */
   @Prop()
   issueID: any;
 }
